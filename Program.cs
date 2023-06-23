b7220647dae1768784205714cdb512d8f16ed46c
Anandmohanty <mohantyanand3@gmail.com>
2023-06-23 09:11:04 +0530

    [Anand] Adding Welcome message

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace TestProject290Batch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Test Project");
+            Console.ReadLine();
+        }
+    }
+}
